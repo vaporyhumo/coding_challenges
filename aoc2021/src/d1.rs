22173d3c8fb8b4cce3933f39f060289e6afc1ac5
vaporyhumo <roanvilina@gmail.com>
2024-03-31 15:30:35 -0300

    Add `aoc2021`

---
+++ aoc2021/src/d1.rs
@@ -0,0 +1,37 @@
+use std::{
+  fs::File,
+  io::{self, BufRead},
+};
+
+pub fn d1e1() {
+  let result = count_increases(read_input());
+
+  println!("{result:#?}");
+}
+
+pub fn d1e2() {
+  let result = count_increases(group_by_3(read_input()));
+
+  println!("{result:#?}");
+}
+
+fn read_input() -> Vec<usize> {
+  io::BufReader::new(File::open("input.txt").unwrap())
+    .lines()
+    .map(|line| line.unwrap().parse::<usize>().unwrap())
+    .collect()
+}
+
+fn count_increases(numbers: Vec<usize>) -> usize {
+  numbers
+    .windows(2)
+    .filter(|window| window[0] < window[1])
+    .count()
+}
+
+fn group_by_3(numbers: Vec<usize>) -> Vec<usize> {
+  numbers
+    .windows(3)
+    .map(|window| window.iter().sum())
+    .collect()
+}
---
+++ aoc2021/src/main.rs
@@ -0,0 +1,9 @@
+mod d1;
+
+use d1::{d1e1, d1e2};
+
+fn main() {
+  d1e1();
+  d1e2();
+}
+
