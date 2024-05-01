691c283fedf5ca110d014bd8c6b614f9d79998ba
vaporyhumo <roanvilina@gmail.com>
2024-04-30 22:18:26 -0400

    Update `aoc2021`

---
+++ aoc2021/src/d3.rs
@@ -0,0 +1,5 @@
+pub fn d3e1() {
+}
+
+pub fn d3e2() {
+}
--- aoc2021/src/main.rs
+++ aoc2021/src/main.rs
@@ -1,9 +1,11 @@
 mod d1;
 mod d2;
+mod d3;
 
 use {
   d1::{d1e1, d1e2},
   d2::{d2e1, d2e2},
+  d3::{d3e1, d3e2},
 };
 
 fn main() {
@@ -11,4 +13,6 @@ fn main() {
   d1e2();
   d2e1();
   d2e2();
+  d3e1();
+  d3e2();
 }
